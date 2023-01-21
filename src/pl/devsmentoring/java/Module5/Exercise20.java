fd79d347c29e3a324c8fdd7da03dcda61a6229a0
cywka333 <114658260+cywka333@users.noreply.github.com>
2023-01-21 12:34:31 +0100

    My final version of exercise 20 from Module 5.

--- src/pl/devsmentoring/java/Module5/Exercise20.java
+++ src/pl/devsmentoring/java/Module5/Exercise20.java
@@ -5,48 +5,46 @@ import java.util.*;
 public class Exercise20 {
     public static void main(String[] args) {
 
-        List<String> colourList = new ArrayList<>(Arrays.asList("zielony", "czerwony", "niebieski", "czarny",
+        List<String> colourList = Arrays.asList("zielony", "czerwony", "niebieski", "czarny",
                 "fioletowy", "granatowy", "niebieski",
                 "czarny", "czarny", "zielony", "cytrynowy", "granatowy",
-                "niebieski", "indygo", "zielony", "czerwony"));
-
-        int greenCounter = Collections.frequency(colourList, "zielony");
-        int redCounter = Collections.frequency(colourList, "czerwony");
-        int blueCounter = Collections.frequency(colourList, "niebieski");
-        int blackCounter = Collections.frequency(colourList, "czarny");
-        int purpleCounter = Collections.frequency(colourList, "fioletowy");
-        int navyCounter = Collections.frequency(colourList, "granatowy");
-        int lemonCounter = Collections.frequency(colourList, "cytrynowy");
-        int indygoCounter = Collections.frequency(colourList, "indygo");
-
-        System.out.println("Numbers of occurrences of colours in list: " + "\nZielony: " + greenCounter +
-                            "\nCzerwony: " + redCounter + "\nNiebieski: " + blueCounter +  "\nCzarny: " + blackCounter +
-                            "\nFioletowy: " + purpleCounter + "\nGranatowy: " + navyCounter + "\nCytrynowy: " + lemonCounter +
-                            "\nIndygo: " + indygoCounter);
-
+                "niebieski", "indygo", "zielony", "czerwony");
 
         System.out.println("Original length of our colour list was: " + colourList.size());
+
+
+        Map<String, Integer> colorCounts = new HashMap<>();
+        for (String color : colourList) {
+            colorCounts.putIfAbsent(color, 0);
+            colorCounts.put(color, colorCounts.get(color) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : colorCounts.entrySet()) {
+            System.out.println("Colour numbers of occurrences: " + entry.getKey() + " - " + entry.getValue());
+        }
+
 
         Set<String> colourAsValues = new HashSet<>(colourList);
         System.out.println("In the list we have: " + colourAsValues.size() + " different colours.\n");
 
-        for (String printingColours : colourAsValues){
+        for (String printingColours : colourAsValues) {
             System.out.println(printingColours);
         }
 
         System.out.println(" ");
 
         colourAsValues.add("purpurowy");
-        for (String printingColours : colourAsValues){
+        for (String printingColours : colourAsValues) {
             System.out.print(printingColours + "; ");
         }
 
         System.out.println(" ");
 
         colourAsValues.remove("niebieski");
-        for (String printingColours : colourAsValues){
+        for (String printingColours : colourAsValues) {
             System.out.print(printingColours + "; ");
         }
 
     }
+
 }
