fe2f65ec690277d0ec78325ff5af27ba68ee8bbf
cywka333 <114658260+cywka333@users.noreply.github.com>
2023-01-27 16:20:34 +0100

    I did exercise 4

--- src/pl/devsmentoring/java/Module9/Exercise4/CalculationSystem.java
+++ src/pl/devsmentoring/java/Module9/Exercise4/CalculationSystem.java
@@ -3,40 +3,25 @@ package pl.devsmentoring.java.Module9.Exercise4;
 class CalculationSystem {
     private Calculator calculator;
 
-    public CalculationSystem (Calculator calculator){
+    public CalculationSystem(Calculator calculator) {
         this.calculator = calculator;
     }
 
-    public void performCalculations(int a, int b){
+    public void performCalculations(int a, int b) {
         System.out.println("Substract: " + calculator.substract(a, b));
         System.out.println("Add: " + calculator.add(a, b));
         System.out.println("Multiply: " + calculator.multiply(a, b));
-        System.out.println("Divide: " + calculator.divide(a, b));
+        if (b == 0){
+            System.out.println("You can not divide by 0!");
+        }else{
+            System.out.println("Divide: " + calculator.divide(a, b));
+        }
+
     }
 
-    Calculator calculatorImplementation = new Calculator() {
-        @Override
-        public int substract(int a, int b) {
-            return a-b;
-        }
-
-        @Override
-        public int add(int a, int b) {
-            return a+b;
-        }
-
-        @Override
-        public int multiply(int a, int b) {
-            return a*b;
-        }
-
-        @Override
-        public double divide(int a, int b) {
-            return a/b;
-        }
-    };
-
 }
+
+
 
 
 
